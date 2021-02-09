058c12cacf453f7f53d2c73225596c3137dfa4b7
Elijah Simpson <elijahsimpson1991@gmail.com>
2021-02-09 13:40:53 -0500

    added graph function

--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,179 +1,187 @@
-import React, { useState, useEffect } from 'react'
-import WeekUtils from 'week-utils'
-import { Link } from 'react-router-dom';
-import { VictoryPie } from 'victory'
+import React, { useState, useEffect } from "react";
+import WeekUtils from "week-utils";
+import { Link } from "react-router-dom";
+import { VictoryPie } from "victory";
 
-import TokenService from '../../services/tokenService'
-import appApiService from '../../services/appApiService'
+import TokenService from "../../services/tokenService";
+import appApiService from "../../services/appApiService";
 
-import './Dashboard.css'
+import "./Dashboard.css";
 
 export default function Dashboard(props) {
+  // GLOBAL STATE & VARIABLES //
 
-    // GLOBAL STATE & VARIABLES //
+  const [tips, setTips] = useState([]);
+  const currentDate = new Date();
+  const weekUtils = new WeekUtils(0, 6);
 
-    const [tips, setTips] = useState([]);
-    const currentDate = new Date();
-    const weekUtils = new WeekUtils(0, 6);
+  // SERVER CALLS //
 
-    // SERVER CALLS //
+  useEffect(() => {
+    appApiService.getAllTips().then((tips) => {
+      setTips(tips);
+    });
+  }, []);
 
-    useEffect(() => {
-        appApiService.getAllTips().then((tips) => {
-            setTips(tips)
-        })
-    }, [])
+  // FUNCTIONS FOR EARNINGS DIV //
 
-    // FUNCTIONS FOR EARNINGS DIV //
-
-    const sumOfTotalEarnings = (tips) => {
-        let sum = 0
-        for(let i = 0; i < tips.length; i++) {
-            let curr = parseFloat(tips[i].tip_total);
-            sum = sum + curr;
-        } return sum;
+  const sumOfTotalEarnings = (tips) => {
+    let sum = 0;
+    for (let i = 0; i < tips.length; i++) {
+      let curr = parseFloat(tips[i].tip_total);
+      sum = sum + curr;
     }
+    return sum;
+  };
 
-    const sumOfYearlyEarnings = (tips) => {
-        let currentYear = currentDate.getFullYear()
-        let sum = 0
-        for(let i = 0; i < tips.length; i++) {
-            let tipDate = new Date(tips[i].tip_date);
-            if(tipDate.getFullYear() === currentYear) {
-                let curr = parseFloat(tips[i].tip_total);
-                sum = sum + curr;
-            }
-        } return sum
+  const sumOfYearlyEarnings = (tips) => {
+    let currentYear = currentDate.getFullYear();
+    let sum = 0;
+    for (let i = 0; i < tips.length; i++) {
+      let tipDate = new Date(tips[i].tip_date);
+      if (tipDate.getFullYear() === currentYear) {
+        let curr = parseFloat(tips[i].tip_total);
+        sum = sum + curr;
+      }
     }
-    
-    const sumOfMonthyEarnings = (tips) => {
-        let currentMonth = currentDate.getMonth()
-        let sum = 0
-        for(let i = 0; i < tips.length; i++) {
-            let tipDate = new Date(tips[i].tip_date);
-            if(tipDate.getMonth() === currentMonth) {
-                let curr = parseFloat(tips[i].tip_total);
-                sum = sum + curr;
-            }
-        } return sum
-    }
-    
-    const sumOfWeeklyEarnings = (tips) => {
-        let currentWeek = weekUtils.curWeek(currentDate)
-        let sum = 0
-        for(let i = 0; i < tips.length; i++) {
-            let tipDate = new Date(tips[i].tip_date);
-            let tipWeek = weekUtils.curWeek(tipDate)
-            if(tipWeek === currentWeek) {
-                let curr = parseFloat(tips[i].tip_total);
-                sum = sum + curr;
-            }
-        } return sum
-    }
+    return sum;
+  };
 
-    // FUNCTIONS FOR GRAPH DATA //
+  const sumOfMonthyEarnings = (tips) => {
+    let currentMonth = currentDate.getMonth();
+    let sum = 0;
+    for (let i = 0; i < tips.length; i++) {
+      let tipDate = new Date(tips[i].tip_date);
+      if (tipDate.getMonth() === currentMonth) {
+        let curr = parseFloat(tips[i].tip_total);
+        sum = sum + curr;
+      }
+    }
+    return sum;
+  };
 
-    const getMonthData = (tips) => {
-        //Add each tip to the same month in the months object
-        //Return the months object
-        let currentYear = currentDate.getFullYear()
-        let months = {
-            "January": 0,
-            "February": 0,
-            "March": 0,
-            "April": 0,
-            "May": 0,
-            "June": 0,
-            "July": 0,
-            "August": 0,
-            "September": 0,
-            "October": 0,
-            "November": 0,
-            "December": 0,
+  const sumOfWeeklyEarnings = (tips) => {
+    let currentWeek = weekUtils.curWeek(currentDate);
+    let sum = 0;
+    for (let i = 0; i < tips.length; i++) {
+      let tipDate = new Date(tips[i].tip_date);
+      let tipWeek = weekUtils.curWeek(tipDate);
+      if (tipWeek === currentWeek) {
+        let curr = parseFloat(tips[i].tip_total);
+        sum = sum + curr;
+      }
+    }
+    return sum;
+  };
+
+  // FUNCTIONS FOR GRAPH DATA //
+
+  const getMonthData = (tips) => {
+    //Add each tip to the same month in the months object
+    //Return the months object
+    let currentYear = currentDate.getFullYear();
+    let months = [
+      { month: "January", sum: 0 },
+      { month: "February", sum: 0 },
+      { month: "March", sum: 0 },
+      { month: "April", sum: 0 },
+      { month: "May", sum: 0 },
+      { month: "June", sum: 0 },
+      { month: "July", sum: 0 },
+      { month: "August", sum: 0 },
+      { month: "September", sum: 0 },
+      { month: "October", sum: 0 },
+      { month: "November", sum: 0 },
+      { month: "December", sum: 0 },
+    ];
+    for (let i = 0; i < tips.length; i++) {
+      let tipDate = new Date(tips[i].tip_date);
+      let tipMonth = tipDate.toLocaleString("default", { month: "long" });
+      if (tipDate.getFullYear() === currentYear) {
+        for (let j = 0; j < months.length; j++) {
+          if (tipMonth === months[j].month) {
+            months[j].sum += parseFloat(tips[i].tip_total);
+          }
         }
-        for(let i = 0; i < tips.length; i++) {
-            let tipDate = new Date(tips[i].tip_date)
-            let tipMonth = tipDate.toLocaleString('default', { month: 'long' })
-            if (tipDate.getFullYear() === currentYear) {
-                months[tipMonth] += parseFloat(tips[i].tip_total)
-            }
-        }
-        return months
+      }
     }
+    return months;
+  };
 
-    const getDateData = (tips) => {
+  const arrangeMonthDate = () => {
+      const data = getMonthData(tips)
+      
+  }
 
-    }
+  const getDateData = (tips) => {};
 
-    const getDayData = (tips) => {
+  const getDayData = (tips) => {};
 
-    }
+  // DATA SETS FOR GRAPHS //
 
-    // DATA SETS FOR GRAPHS //
+  const monthData = getMonthData(tips);
+  const dateData = getDateData(tips);
+  const dayData = getDayData(tips);
 
-    const monthData = getMonthData(tips)
-    const dateData = getDateData(tips)
-    const dayData = getDayData(tips)
-    
-    // BUTTON FUNCTIONS //
+  // BUTTON FUNCTIONS //
 
-    const handleLogout = () => {
-        TokenService.clearAuthToken()
-        props.setUserId(null)
-    }
+  const handleLogout = () => {
+    TokenService.clearAuthToken();
+    props.setUserId(null);
+  };
 
-    // CONSOLE LOG TEST (DELETE BEFORE PRODUCTION BUILD) //
-    console.log(tips)
-    // console.log(currentDate)
-    // console.log(tipDate)
-    // console.log(currentYear)
-    // console.log(currentDate.toLocaleString('default', { month: 'long' }))
-    console.log(monthData)
+  // CONSOLE LOG TEST (DELETE BEFORE PRODUCTION BUILD) //
+  console.log(tips);
+  // console.log(currentDate)
+  // console.log(tipDate)
+  // console.log(currentYear)
+  // console.log(currentDate.toLocaleString('default', { month: 'long' }))
+  console.log(monthData);
 
-    return (
-        <>
-        <h3 className="date">{currentDate.toLocaleDateString()}</h3>
+  return (
+    <>
+      <h3 className="date">{currentDate.toLocaleDateString()}</h3>
 
-        <div className="button-section">
-        <Link to='/tips'><button className="space">Enter Tips</button></Link>
+      <div className="button-section">
+        <Link to="/tips">
+          <button className="space">Enter Tips</button>
+        </Link>
         <button onClick={handleLogout}>Log Out</button>
-        </div>
+      </div>
 
-        <hr/>
+      <hr />
 
-        <div className="earnings">
+      <div className="earnings">
         <span>Total Earnings (All Time): ${sumOfTotalEarnings(tips)}</span>
         <p>Current Yearly Earnings: ${sumOfYearlyEarnings(tips)}</p>
         <p>Current Monthly Earnings: ${sumOfMonthyEarnings(tips)}</p>
         <p>Current Weekly Earnings: ${sumOfWeeklyEarnings(tips)}</p>
-        </div>
+      </div>
 
-        <hr/>
+      <hr />
 
-        <div className="graphs">
+      <div className="graphs">
         <select name="graphs" id="graphs">
-            <option value="year">All Year Totals</option>
-            <option value="month">Highest Earning Months</option>
-            <option value="dates">Highest Earning Dates</option>
-            <option value="days">Hightest Earning Days</option>
+          <option value="year">All Year Totals</option>
+          <option value="month">Highest Earning Months</option>
+          <option value="dates">Highest Earning Dates</option>
+          <option value="days">Hightest Earning Days</option>
         </select>
-        </div>
+      </div>
 
+      {/* All Year Totals Chart */}
 
-        {/* All Year Totals Chart */}
+      {/* Highest Earning Months Chart */}
 
-        {/* Highest Earning Months Chart */}
+      {/* Highest Earning Dates Chart */}
 
-        {/* Highest Earning Dates Chart */}
+      {/* Highest Earning Days Of The Week Chart */}
 
-        {/* Highest Earning Days Of The Week Chart */}
-
-        <VictoryPie
-        data={monthData}
-        x="tip_total"
-        y=""
-        />
-        </>
-        
-    )
+      <VictoryPie 
+      colorScale={["auqamarine", "blueviolet", "blue", "burlywood", "chocolate", "chartreuse", "darkolivegreen", "darkslategrey", "floralwhite", "indianred", "lemonchiffon", "pink"]}
+      data={monthData} 
+      x="month" 
+      y="sum" />
+    </>
+  );
 }
