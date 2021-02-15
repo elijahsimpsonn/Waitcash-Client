c785ccb9228fe69c772a6c4e3993104d14daa771
Elijah Simpson <elijahsimpson1991@gmail.com>
2021-02-15 12:38:19 -0500

    added server link

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: 'https://frozen-hamlet-56051.herokuapp.com/api'
 };
